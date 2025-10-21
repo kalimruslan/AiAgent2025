0296e110702849647207e91834aa311645c881d8
Калимуллин Руслан <rkalimullin@synergy.ru>
2025-10-21 16:01:08 +0300

    День 5 челленджа. Версии моделей

---
+++ llm-app/src/androidMain/kotlin/ru/llm/agent/common/MainActivity.kt
@@ -0,0 +1,19 @@
+package ru.llm.agent
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import ru.llm.agent.common.StartApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        setContent {
+            StartApp()
+        }
+    }
+}
